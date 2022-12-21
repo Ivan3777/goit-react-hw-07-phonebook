23d050e9390edbbfd2b6931e693f4e90d4edeb0d
Ivan Vodopianov <vis3010@gmail.com>
2022-12-21 18:05:10 +0200

    commit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from '@reduxjs/toolkit';
 
 export const getContacts = state => state.contacts.items;
 
@@ -6,9 +6,12 @@ export const getFilter = state => state.filter;
 
 export const getIsLoading = state => state.contacts.isLoading;
 
-export const getFilteredContacts = createSelector([getContacts, getFilter], (contacts, filter) => {
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
     const filterContactsList = contacts.filter(contact => {
-        return contact.name.toLowerCase().includes(filter.toLowerCase());
-      });
-      return filterContactsList;
-})
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filterContactsList;
+  }
+);
